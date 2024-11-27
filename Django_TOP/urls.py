cf3c59cca2c3122e13dda7b25ae89a672b9e38ac
Nastyamind <rasul.nazmiev@mail.ru>
2024-11-27 18:37:43 +0300

    base_practical_work

--- Django_TOP/urls.py
+++ Django_TOP/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     3. Все хватит.
+    4/asd
 """
 from django.contrib import admin
 from django.urls import path, include
